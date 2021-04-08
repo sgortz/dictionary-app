bbfff37dd2c6be6eb7ba53a71ab1ac5f462b8a31
sgortz <sabrina.gortz@gmail.com>
2021-04-08 15:49:54 -0500

    Cleaned comments

--- src/Components/Meanings.js
+++ src/Components/Meanings.js
@@ -8,7 +8,7 @@ function Meanings(props) {
       {props.data.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <ul >
+            <ul>
               <li>{definition.definition}</li>
               <li className="example">{definition.example}</li>
             </ul>
@@ -20,24 +20,3 @@ function Meanings(props) {
 }
 
 export default Meanings;
-
-// meaning.definitions[0].definition
-
-{
-  /* <ul className="examples">
-            <li>the awesome power of the atomic bomb</li>
-          </ul>
-          <h6>adverb</h6>
-          <ul className="part-of-speech">
-            <li>Extremely well; excellently.</li>
-          </ul>
-          <h6>adjective</h6>
-          <ul className="part-of-speech">
-            <li>
-              Extremely impressive or daunting; inspiring great admiration,
-              apprehension, or fear.
-            </li>
-          </ul>
-
-           */
-}
