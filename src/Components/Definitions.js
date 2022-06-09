99558bb9882ad4ef227ae846053680f5e214d3c8
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-06-09 15:54:53 -0400

    clean unecessary divs

--- src/Components/Definitions.js
+++ src/Components/Definitions.js
@@ -5,29 +5,27 @@ import "./Definitions.css";
 function Definitions(props) {
   if (props.data) {
     return (
-      <div className="Definitions">
-        <div className="light-green">
-          <h2 className="main-word">{props.data.word}</h2>
-          <small className="phonetics">{props.data.phonetics[0].text}</small>
-          {"   "}
-          <a
-            href={props.data.phonetics[0].audio}
-            target="_blank"
-            className="audio"
-            rel="noreferrer"
-          >
-            <i className="fas fa-volume-up"></i>
-          </a>
-          <br />
-          {props.data.meanings.map(function (meaning, index) {
-            return (
-              <div key={index}>
-                <Meanings data={meaning} />
-              </div>
-            );
-          })}
-          <br />
-        </div>
+      <div className="light-green">
+        <h2 className="main-word">{props.data.word}</h2>
+        <small className="phonetics">{props.data.phonetics[0].text}</small>
+        {"   "}
+        <a
+          href={props.data.phonetics[0].audio}
+          target="_blank"
+          className="audio"
+          rel="noreferrer"
+        >
+          <i className="fas fa-volume-up"></i>
+        </a>
+        <br />
+        {props.data.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meanings data={meaning} />
+            </div>
+          );
+        })}
+        <br />
       </div>
     );
   } else {
