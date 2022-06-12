7132ad53f9a6ff4cfa33da0582f8fc37e3a689d3
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-06-12 18:14:52 -0400

    Add synonyms to each part of speech

--- src/Components/Meanings.js
+++ src/Components/Meanings.js
@@ -2,19 +2,23 @@ import Synonyms from "./Synonyms";
 import "./Meanings.css";
 
 function Meanings(props) {
+  console.log('is there meaning: ', props)
   return (
     <div className="Meanings">
-      <h6 className="part-of-speech">{props.data.partOfSpeech}</h6>
+      <h5 className="part-of-speech">{props.data.partOfSpeech}</h5>
       {props.data.definitions.map((definition, index) => (
         <div key={index}>
           <ul>
             <li>{index + 1}.{" "} {definition.definition}</li>
             <li className={definition.example ? "example" : "inactive"}> Example: {definition.example}</li>
           </ul>
-          {definition.synonyms ? <Synonyms data={definition.synonyms.length} /> : null}
+          {definition.synonyms ? <Synonyms data={definition.synonyms} /> : null}
+          {/* {definition.antonyms ? <Antonyms data={definition.antonyms.length} /> : null} */}
         </div>
       )
       )}
+         {props.data.synonyms.length > 0 ? <Synonyms synonyms={props.data.synonyms} /> : null}
+      {/* {props.antonyms ? <Antonyms data={props.antonyms} /> : null} */}
     </div>
   );
 }
--- src/Components/Synonyms.js
+++ src/Components/Synonyms.js
@@ -1,17 +1,21 @@
 import "./Synonyms.css";
 
 function Synonyms(props) {
-  if (props.data) {
+  console.log('is there synonyms: ', props);
+  if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-        {props.data.map(function (synonym, index) {
-          return (
-            <li className="word" key={index}>
-              {synonym}
-            </li>
-          );
-        })}
-      </ul>
+      <>
+     <h6 className="Synonym-title">Synonyms</h6>
+        <ul className="Synonyms-list">
+          {props.synonyms.map(function (synonym, index) {
+            return (
+              <li className="word" key={index}>
+                {synonym}
+              </li>
+            );
+          })}
+        </ul>
+      </>
     );
   } else {
     return null;
